2d3618d98ca60b9d45d89ae763327bb234c4c781
f-allian <allianfarhad@gmail.com>
2024-11-04 17:17:29 +0000

    feat: forgot password

--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.views import PasswordResetConfirmView
 from django.views.generic.edit import CreateView, UpdateView
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
 from survey.models import Questionnaire
 from django.shortcuts import render
 from django.views import View
@@ -14,19 +12,19 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 
 class SignupView(CreateView):
     form_class = ManagerSignupForm
     template_name = 'home/register.html'
-    success_url = reverse_lazy('home')
 
-    def get(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect(reverse_lazy('notes.list'))
-        return super().get(request, *args, **kwargs)
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(reverse_lazy('home'))
 
 class LogoutInterfaceView(LogoutView):
     success_url = reverse_lazy('login')
@@ -36,17 +34,9 @@ class LoginInterfaceView(LoginView):
     form_class = ManagerLoginForm
     success_url = reverse_lazy('home')
 
-    def form_valid(self, form):
-        email = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-
-        user = authenticate(request=self.request, username=email, password=password)
-        if user is not None:
-            login(self.request, user)
-            return super().form_valid(form)
-        else:
-            messages.error(self.request, "Invalid email or password.")
-            return self.form_invalid(form)
+    def form_invalid(self, form):
+        messages.error(self.request, "Invalid email or password.")
+        return super().form_invalid(form)
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -93,21 +83,35 @@ class CustomPasswordResetView(PasswordResetView):
             messages.error(request, "Email field is required.")
             return self.get(request, *args, **kwargs)
 
-        try:
-            User.objects.get(email=email)
-            return super().post(request, *args, **kwargs)
-        except User.DoesNotExist:
-            messages.error(request, "No user is associated with this email address.")
-            return self.get(request, *args, **kwargs)
+        return super().post(request, *args, **kwargs)
 
     def form_valid(self, form):
+
         response = super().form_valid(form)
+
         for user in form.get_users(form.cleaned_data['email']):
-            print(f'Debug - user.pk: {user.pk}')
-            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-            token = default_token_generator.make_token(user)
-            print(f'Debug - uidb64: {uidb64}, token: {token}')
-        return response
+            custom_uid = urlsafe_base64_encode(force_bytes(user.pk, encoding='utf-16'))
+            custom_token = default_token_generator.make_token(user)
+
+            context = {
+                'uid': custom_uid,
+                'token': custom_token,
+                'first_name': user.first_name,
+                'username': user.username,
+                'protocol': 'https' if self.request.is_secure() else 'http',
+                'domain': self.request.get_host(),
+            }
+            email_subject = render_to_string(self.subject_template_name, context).strip()
+            email_body = render_to_string(self.email_template_name, context)
+
+            send_mail(
+                subject=email_subject,
+                message=email_body,
+                from_email="no-reply-SORT@sheffield.ac.uk",
+                recipient_list=[user.email],
+            )
+
+        return super().form_valid(form)
 
 
 class CustomPasswordResetDoneView(PasswordResetDoneView):
@@ -118,11 +122,6 @@ class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'home/password_reset_confirm.html'
     success_url = reverse_lazy('password_reset_complete')
 
-    def get(self, request, *args, **kwargs):
-        uidb64 = kwargs.get('uidb64')
-        token = kwargs.get('token')
-        print(f'CustomPasswordResetConfirmView: {uidb64}, uid: {token}, token: {token}')
-        return super().get(request, *args, **kwargs)
 
 
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
