1a6bfdb0aaae2833878ee22b57c1b42ec30edead
f-allian <allianfarhad@gmail.com>
2025-02-12 16:32:59 +0000

    fix urls

--- survey/urls.py
+++ survey/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('completion/', views.CompletionView.as_view(), name='completion_page'),
     path('survey_response/<str:token>', views.SurveyResponseView.as_view(), name='survey_response'),
     path('survey_link_invalid/', views.SurveyLinkInvalidView.as_view(), name='survey_link_invalid'),
-    path('invite/<int:pk>', views.InvitationView.as_view(), name='invite'),
     path('invite/success/', views.SuccessInvitationView.as_view(), name='success_invitation'),
 
 ]
