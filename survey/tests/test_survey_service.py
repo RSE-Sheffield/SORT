d449a7cf02273d9c72d2e243840c108b2eb4232b
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-03-14 11:49:16 +0000

    chore(tests): Start adding survey tests

---
+++ survey/tests/test_survey_service.py
@@ -0,0 +1,17 @@
+import django.test
+import django.contrib.auth.models
+from survey.services import SurveyService
+
+from survey.models import Survey
+
+
+class SurveyServiceTestCase(django.test.TestCase):
+    def setUp(self):
+        self.service = SurveyService()
+
+    def test_can_view(self):
+        """
+        Anonymous user can view a survey
+        """
+        anonymous_user = django.contrib.auth.models.AnonymousUser()
+        self.assertTrue(self.service.can_view(user=anonymous_user, instance=Survey.objects.create()))
