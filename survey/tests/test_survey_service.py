67035d2a76c975fb454c3743f25e4b86cd4f6d0b
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-03-19 12:16:15 +0000

    Fix survey service logic

--- survey/tests/test_survey_service.py
+++ survey/tests/test_survey_service.py
@@ -8,10 +8,19 @@ from survey.models import Survey
 class SurveyServiceTestCase(django.test.TestCase):
     def setUp(self):
         self.service = SurveyService()
+        self.survey = Survey.objects.create()
 
-    def test_can_view(self):
+    def test_anonymous_view(self):
         """
-        Anonymous user can view a survey
+        Anonymous user can't view a survey
         """
         anonymous_user = django.contrib.auth.models.AnonymousUser()
-        self.assertTrue(self.service.can_view(user=anonymous_user, survey=Survey.objects.create()))
+
+        self.assertFalse(self.service.can_view(user=anonymous_user, survey=self.survey))
+
+    def test_user_can_view(self):
+        """
+        A user in that organisation can view a survey
+        """
+
+        pass
