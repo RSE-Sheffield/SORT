904ad3d1b07b6416159a6d953832c874e46f8eba
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-03-27 14:12:58 +0000

    Improve project views test code quality

--- home/tests/test_project_views.py
+++ home/tests/test_project_views.py
@@ -2,37 +2,25 @@
 Test project views
 """
 
-from http import HTTPStatus
-
-import django.test
-import django.urls
-from django.contrib.auth import get_user_model
-
 import SORT.test.test_case
-from home.services import OrganisationService, ProjectService
-
-User = get_user_model()
+from SORT.test.model_factory import ProjectFactory
 
 
 class ProjectViewTestCase(SORT.test.test_case.ViewTestCase):
 
     def setUp(self):
         super().setUp()
-        self.project_service = ProjectService()
-        self.organisation_service = OrganisationService()
+        self.project = ProjectFactory()
+        self.user = self.project.organisation.members.first()
 
-        self.organisation = self.organisation_service.create_organisation(
-            user=self.superuser,
-            name="My organisation",
-            description="This is an org.",
-        )
-        self.project = self.project_service.create_project(
-            user=self.superuser, name="My test project", organisation=self.organisation
-        )
+    def test_project_detail_view(self):
+        self.get("project", project_id=self.project.pk)
 
-    def test_projects_detail_view(self):
-        self.login_superuser()
-        response = self.client.get(
-            django.urls.reverse("project", kwargs=dict(project_id=self.project.pk))
-        )
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_project_edit_view(self):
+        self.skipTest("Not implemented")
+
+    def test_project_create_view(self):
+        self.skipTest("Not implemented")
+
+    def test_project_delete_view(self):
+        self.skipTest("Not implemented")
