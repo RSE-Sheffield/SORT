58069497eba92fee9b0121f3ede83ed7893ca71b
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-04-03 15:20:20 +0100

    Code tidy

--- assets/sort-survey-configurator/src/App.svelte
+++ assets/sort-survey-configurator/src/App.svelte
@@ -14,10 +14,9 @@
     //otherwise use the default config
     let elem = document.getElementById("configData");
     let initConfigData = {};
-    if(elem){
+    if (elem) {
         initConfigData = JSON.parse(elem.textContent);
-    }
-    else{
+    } else {
         initConfigData = defaultConfigs;
     }
     let configData = $state(_.cloneDeep(initConfigData));
@@ -27,8 +26,6 @@
     let editable = $state(true);
 
     let responseValue = $state();
-
-
 
 
     function getConfig() {
@@ -44,9 +41,9 @@
     }
 
 
-    let page = $state(0);
-
+    let page: int = $state(0);
 </script>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
     <div class="container-fluid">
         <h2>SORT UI Dev test page</h2>
@@ -54,43 +51,46 @@
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="/" onclick={(e)=> {e.preventDefault(); e.stopPropagation(); page = 0;}}>Main</a>
+                <a class="nav-link" href="/"
+                   onclick={(e)=> {e.preventDefault(); e.stopPropagation(); page = 0;}}>Main</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/" onclick={(e)=>{e.preventDefault(); e.stopPropagation(); page = 1;}}>Consent demography</a>
+                <a class="nav-link" href="/" onclick={(e)=>{e.preventDefault(); e.stopPropagation(); page = 1;}}>
+                    Consent demography
+                </a>
             </li>
         </ul>
     </div>
 </nav>
 <main>
     {#if page == 0}
-    <div class="container">
-        <div class="btn-group mb-3" role="group" aria-label="Test functions">
-            <button class="btn btn-primary" onclick={getConfig}>Get config</button>
-            <button class="btn btn-primary" onclick={toggleEditable}>
-                {#if editable}
-                    Turn off editing
-                {:else}
-                    Turn on editing
-                {/if}
-            </button>
-            <button class="btn btn-primary" onclick={getValues}>Get values</button>
-        </div>
+        <div class="container">
+            <div class="btn-group mb-3" role="group" aria-label="Test functions">
+                <button class="btn btn-primary" onclick={getConfig}>Get config</button>
+                <button class="btn btn-primary" onclick={toggleEditable}>
+                    {#if editable}
+                        Turn off editing
+                    {:else}
+                        Turn on editing
+                    {/if}
+                </button>
+                <button class="btn btn-primary" onclick={getValues}>Get values</button>
+            </div>
 
-        {#if editable}
-            <SurveyConfigurator bind:config={configData}
-                                bind:this={surveyComponent}
-                                editable={editable}
-            />
-        {:else}
-            <SurveyResponse config={configData} bind:value={responseValue} bind:this={surveyComponent}/>
-        {/if}
-    </div>
+            {#if editable}
+                <SurveyConfigurator bind:config={configData}
+                                    bind:this={surveyComponent}
+                                    editable={editable}
+                />
+            {:else}
+                <SurveyResponse config={configData} bind:value={responseValue} bind:this={surveyComponent}/>
+            {/if}
+        </div>
     {:else if page == 1}
 
-    <div class="container">
-        <SurveyConfigConsentDemographyApp></SurveyConfigConsentDemographyApp>
-    </div>
+        <div class="container">
+            <SurveyConfigConsentDemographyApp></SurveyConfigConsentDemographyApp>
+        </div>
     {/if}
 
 </main>
