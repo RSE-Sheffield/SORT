a08b7f975abb2be9cd8b1fe7da1d6b3c6cf4cede
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-06-02 15:45:22 +0100

    Use User.email as username field

--- SORT/settings.py
+++ SORT/settings.py
@@ -245,3 +245,4 @@ SURVEY_TEMPLATES = {
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*", "password2*"]
 ACCOUNT_LOGIN_METHODS = {"email"}
+ACCOUNT_USER_MODEL_USERNAME_FIELD = "email"
