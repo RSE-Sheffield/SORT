904dda1cbb599a3759d7a1fa98f6192e24551246
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-07-14 14:11:58 +0100

    Fix system check registration

--- home/apps.py
+++ home/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
+
+    def ready(self):
+        """
+        Initialise this app
+        """
+        # Load system checks
+        # https://docs.djangoproject.com/en/5.2/topics/checks/
+        # Suppress linting errors
+        import home.checks  # noqa: F401
