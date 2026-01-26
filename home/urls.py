28b8d9810179cf26425acc029a0dd9f611367f4b
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2026-01-26 17:12:41 +0000

    feat: Add FAQs page (#465)

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,5 @@
 __author__ = "Farhad Allian"
 
-
 import django.urls
 from django.urls import path, re_path
 
@@ -107,6 +106,7 @@ urlpatterns = [
         views.TroubleshootingView.as_view(),
         name="troubleshooting",
     ),
+    path("help/faq/", views.FAQView.as_view(), name="faq", ),
     path("eula/", views.LicenseAgreementView.as_view(), name="eula"),
     path("privacy/", views.PrivacyPolicyView.as_view(), name="privacy"),
     path(
